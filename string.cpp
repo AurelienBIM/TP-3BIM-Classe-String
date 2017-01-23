5451fe73274f8f5531b523ffe5b8017140c61ebc
Damien <damien.agopian@insa-lyon.fr>
2017-01-23 15:45:43 +0100

    Constructeurs de copie

--- string.cpp
+++ string.cpp
@@ -7,3 +7,23 @@ string::string (){
 	capacity_ = 0;
 	chaine = nullptr;
 }
+
+string::string (const char* ch){
+  char c = ch[0];
+  unsigned int longueur = 0;
+  while(c != '\0'){
+    longueur ++;
+    c = ch[longueur];
+  }
+  if (longueur > MAX_SIZE){
+    longueur = MAX_SIZE;
+    std::cout << "Erreur : Chaine trop longue"  << std::endl;
+  }
+  size_ = longueur;
+  capacity_ = longueur;
+  chaine = new char[longueur + 1];
+  for(unsigned int i = 0 ; i < longueur ; i++){
+    chaine[i] = ch[i];
+  }
+  chaine[longueur] = '\0';
+}
